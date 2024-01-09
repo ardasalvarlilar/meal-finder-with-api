10f88ddccc803a387fd4e9d84f7471cc1eff0a27
Arda Şalvarlılar <arda4salvar7@gmail.com>
2024-01-10 00:18:09 +0300

    half js

---
+++ script.js
@@ -0,0 +1,81 @@
+const search = document.getElementById('search'),
+  submit = document.getElementById('submit'),
+  random = document.getElementById('random'),
+  mealsEl = document.getElementById('meals'),
+  resultHeading = document.getElementById('result-heading'),
+  single_mealEl = document.getElementById('single-meal');
+
+function searchMeal(e){
+  e.preventDefault()
+  single_mealEl.innerHTML = ''
+  const term = search.value
+
+  if(term.trim()){
+    fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${term}`)
+      .then(res => res.json())
+      .then(data => {
+        console.log(data)
+        resultHeading.innerHTML = `<h2>Search results for '${term}': </h2>`
+        if(data.meals === null){
+          resultHeading.innerHTML = `<p>There are no search results. Try again.</p>`
+        }else{
+          mealsEl.innerHTML = data.meals.map(meal => `
+            <div class="meal">
+              <img src="${meal.strMealThumb}" alt="${meal.strMeal}" />
+              <div class="meal-info" data-mealId="${meal.idMeal}">
+                <h3>${meal.strMeal}</h3>
+              </div>
+            </div>
+          `).join('')
+        }
+      })
+      search.value = ''
+  }else{
+    alert('please enter something')
+  }
+}
+
+function getMealById(mealID){
+  fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealID}`)
+    .then(res => res.json())
+    .then(data => {
+      const meal = data.meals[0]
+
+      addMealToDom(meal)
+    })
+}
+
+function addMealToDom(meal){
+  const ingredients = []
+  for(let i = 1; i<=20; i++){
+    if(meal[`strIngredient${i}`]){
+      ingredients.push(`${meal[`strIngredient${i}`]} - ${meal[`strMeasure${i}`]}`)
+    }else{
+      break
+    }
+  }
+  single_mealEl.innerHTML = `
+    <div class="single-meal">
+      <h1>${meal.strMeal}</h1>
+      <img src="${meal.strMealThumb}" alt="${meal.strMeal}" />
+      <div class="single-meal-info">
+        ${meal.strCategory ? `<p>${meal.strCategory}</p>` : ''}
+        ${meal.strAria ? `<p>${meal.strAria}</p>` : ''}
+      </div>
+      <div class="main">
+        <p>${meal.strInstructions}</p>
+        <h2>Ingredients</h2>
+        <ul>
+          ${ingredients.map(ing => `<li>${ing}</li>`).join('')}
+        </ul>
+      </div>
+    </div>
+  `
+}
+
+// events
+submit.addEventListener('submit',searchMeal)
+mealsEl.addEventListener('click', e => {
+  const mealID = e.target.closest('.meal-info').dataset.mealid;
+  getMealById(mealID);
+});
